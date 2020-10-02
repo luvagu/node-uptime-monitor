f419b150513e0a4fea4ed7479d73a8ba0576b4b3
Luis Vallejo <luiavag@gmail.com>
2020-10-02 17:16:59 -0500

    created a user post request to create the user and hash the password

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const http = require('http')
 const https = require('https')
 const url = require('url')
 const StringDecoder = require('string_decoder').StringDecoder
-const config = require('./config')
+const config = require('./lib/config')
 const fs = require('fs')
 const handlers = require('./lib/handlers')
+const helpers = require('./lib/helpers')
 
 // Instantiate the HTTP server
 const httpServer = http.createServer((req, res) => {
@@ -68,7 +69,7 @@ const unifiedServer = (req, res) => {
             queryString: queryString,
             method: method,
             header: headers,
-            payload: buffer
+            payload: helpers.parseJsonToObject(buffer)
         }
 
         // Route the request to the handler specified in the router
