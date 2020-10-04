fdecbc0775476b4f9536164807cab6c539ea8f75
Luis Vallejo <luiavag@gmail.com>
2020-10-04 00:47:47 -0500

    adding a checks service

--- index.js
+++ index.js
@@ -98,5 +98,6 @@ const unifiedServer = (req, res) => {
 const router = {
     ping: handlers.ping,
     users: handlers.users,
-    tokens: handlers.tokens
+    tokens: handlers.tokens,
+    checks: handlers.checks
 }
