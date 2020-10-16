db26c846cf0e05af6bd14459565bbe09c22cb964
Luis Vallejo <luiavag@gmail.com>
2020-10-16 06:16:08 -0500

    adding a CLI tool to query app data

--- index.js
+++ index.js
@@ -3,9 +3,15 @@
 *
 */
 
+// Utility commands available:
+// SERVER >>> NODE_DEBUG=server node index.js
+// WORKERS >>> NODE_DEBUG=workers node index.js
+// CLI >>> NODE_DEBUG=cli node index.js
+
 // Dependencies
 const server = require('./lib/server')
 const workers = require('./lib/workers')
+const cli = require('./lib/cli')
 
 // Delclare the app
 const app = {}
@@ -17,6 +23,11 @@ app.init = () => {
 
     // Start the workers
     workers.init()
+
+    // Start the CLI, but make sure it starts last
+    setTimeout(() => {
+        cli.init()
+    }, 50)
 }
 
 // Execute
--- public/app.js
+++ public/app.js
@@ -534,10 +534,7 @@ app.loadChecksListPage = () => {
 												? responsePayload.state
 												: 'unknown'
 										td3.innerHTML = state
-										td4.innerHTML =
-											'<a href="/checks/edit?id=' +
-											responsePayload.id +
-											'">View / Edit / Delete</a>'
+										td4.innerHTML = '<a href="/checks/edit?id=' + responsePayload.id + '">View / Edit / Delete</a>'
 									} else {
 										console.log('Error trying to load check ID: ', checkId)
 									}
